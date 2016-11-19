609f36d6e4f861257bc1de662bdfa4b0526ea12d
Jens Jansson <jens.jansson@vaadin.com>
2016-11-19 23:22:51 +0200

    first commit

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
